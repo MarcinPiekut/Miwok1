68a578aa14817329aa07fd4060f5882fb313c296
MarcinPiekut <marcin.piekut@pracowity.pl>
2018-09-03 20:25:01 +0200

    code update

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -33,14 +33,14 @@ public class NumbersActivity extends AppCompatActivity {
 
 
 
-        ArrayAdapter<Word> itemsAdapter = new ArrayAdapter<Word>(this, R.layout.list_item, words);
+       // ArrayAdapter<Word> itemsAdapter = new ArrayAdapter<Word>(this, R.layout.list_item, words);
 
-        /*
+
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
         // adapter knows how to create list items for each item in the list.
         WordAdapter adapter;
         adapter = new WordAdapter(this, words);
-       */
+
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
@@ -49,6 +49,6 @@ public class NumbersActivity extends AppCompatActivity {
 
         // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
         // {@link ListView} will display list items for each {@link Word} in the list.
-        listView.setAdapter((ListAdapter) itemsAdapter);
+        listView.setAdapter((ListAdapter) adapter);
     }
 }
